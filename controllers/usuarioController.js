91859e6c95e275525bb530a932a9ab3d9cb65f31
Ezequiel Barrale <112732@tecnicatura.frc.utn.edu.ar>
2023-04-17 00:05:18 -0300

    put usuarios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -25,9 +25,35 @@ const usuarioGet = async (req = request, res = response) =>{
     
 }
 
+const usuarioPut = async (req, res = response) =>{
 
+    const {id} = req.params
+    const {nombre, apellido, idempresa, nro_documento, email, password, telefono, idrol}= req.body
+
+    try {
+
+        const [result] = await pool.promise().query('UPDATE usuario SET nombre = IFNULL(?,nombre), apellido = IFNULL(?,apellido), idempresa = IFNULL(?,idempresa), nro_documento = IFNULL(?,nro_documento), email = IFNULL(?,email), password = IFNULL(?,password), telefono = IFNULL(?,telefono), idrol = IFNULL(?,idrol) WHERE idusuario = ?', 
+        [nombre, apellido, idempresa, nro_documento, email, password, telefono, idrol, id])
+
+        if (result.affectedRows <= 0) return res.status(404).json({
+            message: 'Usuario no encontrado'
+        })
+
+        const [rows] = await pool.promise().query('SELECT * FROM usuario WHERE idusuario = ?', [id])
+
+        res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
+    
+}
 
 module.exports = {
     usuariosGet,
-    usuarioGet
+    usuarioGet,
+    usuarioPost,
+    usuarioPut,
+    usuarioDelete
 }
