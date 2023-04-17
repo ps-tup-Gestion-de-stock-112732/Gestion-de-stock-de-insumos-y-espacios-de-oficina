6c5e9f09a253649eb554e6f832425d313520eeee
Ezequiel Barrale <112732@tecnicatura.frc.utn.edu.ar>
2023-04-16 23:49:45 -0300

    get usuarios

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors')
+
+class Server{
+    
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas
+        this.routes();
+    }
+
+    middlewares(){
+
+        //CORS
+        this.app.use(cors())
+
+        //Lectura y parse del body
+        this.app.use(express.json());
+    }
+
+    routes(){
+        
+        this.app.use(this.usuariosPath, require('../routes/usuario'))
+
+        this.app.use((req, res, next)=>{
+            res.status(404).json({
+                message: 'endpoint no encontrado'
+            })
+        })
+    }
+
+    listen(){
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port)
+        })
+    }
+
+}
+
+module.exports = Server;
