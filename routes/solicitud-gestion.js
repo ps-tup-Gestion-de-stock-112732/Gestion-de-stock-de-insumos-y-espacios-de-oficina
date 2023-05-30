050853360b94ead1e675397f2dbc303ceeec54ee
Ezequiel Barrale <112732@tecnicatura.frc.utn.edu.ar>
2023-05-30 02:37:13 -0300

    listado detallado de autorizaciones gestion

--- routes/solicitud-gestion.js
+++ routes/solicitud-gestion.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
-const { solicitudGestionPost } = require('../controllers/solicitud-gestionController');
+const { solicitudGestionPost, solicitudesGet, solicitudesFiltroGet, solicitudGet, rechazarSolicitudPut } = require('../controllers/solicitud-gestionController');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { idPedidoValidacionBody } = require('../helpers/db-validators');
+const { idPedidoValidacionBody, empresaValidacion, estadoValidacion, idSolicitudGestionValidacion, idAutorizanteValidacionBody } = require('../helpers/db-validators');
 const { validatJWT } = require('../middlewares/validar-jwt');
 
 const routerSolicitudGestion = Router();
@@ -13,5 +13,35 @@ routerSolicitudGestion.post('/', [
     validarCampos
     ],
     solicitudGestionPost)
+
+routerSolicitudGestion.get('/:idautorizacion', [
+    validatJWT,
+    check('idautorizacion', 'El identificador de solicitud es obligatorio').notEmpty().custom( idSolicitudGestionValidacion ),
+    validarCampos
+    ],
+    solicitudGet)
+
+routerSolicitudGestion.post('/all', [
+    validatJWT,
+    check('idempresa', 'La empresa es obligatoria').notEmpty().custom( empresaValidacion ),
+    check('estado', 'El estado es obligatorio').notEmpty().custom( estadoValidacion ),
+    validarCampos
+    ],
+    solicitudesGet)
+
+routerSolicitudGestion.post('/filtro', [
+    validatJWT,
+    check('idempresa', 'La empresa es obligatoria').notEmpty().custom( empresaValidacion ),
+    validarCampos
+    ],
+    solicitudesFiltroGet)
+
+routerSolicitudGestion.put('/rechazar/:idautorizacion', [
+    validatJWT,
+    check('idautorizacion', 'El identificador de solicitud es obligatorio').notEmpty().custom( idSolicitudGestionValidacion ),
+    check('idautorizante', 'El autorizador es obligatorio').notEmpty().custom( idAutorizanteValidacionBody ),
+    validarCampos
+    ],
+    rechazarSolicitudPut)
 
 module.exports = routerSolicitudGestion;
