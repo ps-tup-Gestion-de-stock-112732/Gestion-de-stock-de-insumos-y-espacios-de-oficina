23a4c98cde255e679863374322edc92debb8fd01
Ezequiel Barrale <112732@tecnicatura.frc.utn.edu.ar>
2023-06-08 01:52:18 -0300

    registrar espacio de oficina

--- helpers/datetime.js
+++ helpers/datetime.js
@@ -23,6 +23,36 @@ function datetime() {
     return year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
 }
 
+function datetimeshort() {
+    let date_ob = new Date();
+
+    // current date
+    // adjust 0 before single digit date
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    // current month
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    // current year
+    let year = date_ob.getFullYear();
+
+    return year + "-" + month + "-" + date
+}
+
+function trimstring(fecha) {
+
+    if (fecha.length > 10) {
+        let caracter = fecha.indexOf('T')
+        let cadena = fecha.slice(0,caracter)
+
+        return cadena
+    }
+    
+    return fecha
+}
+
 module.exports = {
-    datetime
+    datetime,
+    datetimeshort,
+    trimstring
 }
